c7bae0bef8498e93b2cde1a857b11a699124e272
Yogesh Kulkarni <yogesh.kulkarni@perigorddata.com>
2020-03-16 13:12:05 +0530

    Updates

---
+++ precache-manifest.399f90c33df525467573a88395924618.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a95514f35710b5454a09b3f5e51b5e7",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "5183813c6a3a530ff0a3",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f1d840840b4be05055eb",
+    "url": "/react-deploy/static/js/2.d3b1d499.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-deploy/static/js/2.d3b1d499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5183813c6a3a530ff0a3",
+    "url": "/react-deploy/static/js/main.ad4d824e.chunk.js"
+  },
+  {
+    "revision": "fc04b6ae18b669ce4a52",
+    "url": "/react-deploy/static/js/runtime-main.66bdc372.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
